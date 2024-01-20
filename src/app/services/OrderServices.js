919bb23d1a7908409c7603aa0ee894a1c7903cad
Hasiful Islam <nazmulbhuyian000@gmail.com>
2024-01-20 13:39:58 +0600

    ssl commercve add

--- src/app/services/OrderServices.js
+++ src/app/services/OrderServices.js
@@ -10,7 +10,13 @@ exports.getAllOrderService = async (limit, skip) => {
 
 // Find A Order
 exports.getAOrderService = async (email) => {
-    const getAOrderData = await OrderModel.find({email: email}).sort({ "_id": -1 }).populate('userInfo')
+    const getAOrderData = await OrderModel.findOne({email: email}).populate('userInfo')
+    return getAOrderData;
+}
+
+// Find A Order with tnx id
+exports.getAOrderTNXIDService = async (transactionId) => {
+    const getAOrderData = await OrderModel.findOne({transactionId: transactionId}).populate('userInfo')
     return getAOrderData;
 }
 
@@ -48,13 +54,49 @@ exports.postCheckOrderWithCardServices = async (data) => {
     return updateCheck;
 };
 
+// Post a order with card and initially pemding
+
+exports.postInitialOrderServices = async (data) => {
+    const createOrder = await OrderModel.create(data);
+    return createOrder;
+}
+
 
 exports.postOrderWithCardServices = async (data) => {
 
+    const { order } = data;
+
+    const updatePromises = order.map(async (orderItem) => {
+        const updatedSizeVariation = await ProductModel.findOneAndUpdate(
+            {
+                _id: orderItem.productId,
+                'size_variation._id': orderItem.size_variationId,
+            },
+            {
+                $inc: {
+                    'size_variation.$.quantity': -1 * orderItem.quantity,
+                },
+            },
+            {
+                new: true,
+            }
+        );
+
+        return updatedSizeVariation;
+    });
+
+    // Wait for all updates to complete
+    const updatedSizeVariations = await Promise.all(updatePromises);
+
+    return updatedSizeVariations;
+}
+
+// post order with COD
+exports.postOrderWithCODServices = async (data) => {
+
     const createOrder = await OrderModel.create(data);
-    
     if(!createOrder){
-        throw new ApiError(400, 'Order Added Failed !')
+        throw new ApiError(400, 'Order confirm failed !');
     }
 
     const { order } = data;
@@ -109,6 +151,20 @@ exports.deleteOrderWithAddQuantityServices = async (order) => {
     const updatedSizeVariations = await Promise.all(updatePromises);
 
     return updatedSizeVariations;
+}
+
+// update Order when payment success
+exports.updateOrderPaymentSuccessService = async (transactionId) => {
+    const updateCompleteOrderInfo = await OrderModel.findOne({transactionId: transactionId})
+    const completeOrderUpdate = await OrderModel.updateOne(updateCompleteOrderInfo, {type: 'paid', status: 'complete'}, {
+    runValidators: true });
+    return completeOrderUpdate;
+}
+
+// delete Order when payment fail
+exports.deleteOrderPaymentFailService = async (transactionId) => {
+    const deleteOrderInfo = await OrderModel.deleteOne({transactionId: transactionId})
+    return deleteOrderInfo;
 }
 
 
