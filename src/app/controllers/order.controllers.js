95b4bd3b1775341bd2ab3de947d9aad517d2fb23
Hasiful Islam <nazmulbhuyian000@gmail.com>
2024-01-21 16:16:48 +0600

    filter bug fixed

--- src/app/controllers/order.controllers.js
+++ src/app/controllers/order.controllers.js
@@ -83,14 +83,20 @@ const generateOrderID = async() => {
 }
 
 // generate trnx id
-const generateTRNXID = async() => {
-
+const generateTRNXID = async () => {
     let isUnique = false;
     let transactionId;
 
     while (!isUnique) {
+        transactionId = ''; // Initialize transactionId
 
-        transactionId = new ObjectId().toString();
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        const length = 10;
+
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            transactionId += characters.charAt(randomIndex);
+        }
 
         // Check if the generated ID is unique
         const existingOrder = await OrderModel.findOne({ transactionId: transactionId });
@@ -102,7 +108,7 @@ const generateTRNXID = async() => {
     }
 
     return transactionId;
-}
+};
 
 exports.postOrder = async (req, res, next) => {
     try {
--- src/app/services/filter.services.js
+++ src/app/services/filter.services.js
@@ -2,6 +2,26 @@ const ProductModel = require("../models/Product.model");
 
 // find data with query
 exports.getFilterDataServices = async (query) => {
-    const data = await ProductModel.find(query).populate(['menuId', 'categoryId', 'collectionId', 'colorId', 'featureId', 'styleId', 'subCategoryId']);
-    return data;
+
+    const keyword = query.title; // Assuming 'title' is part of the query
+    const titleRegex = new RegExp(keyword, 'i');
+
+
+    const allData = await ProductModel.find({}).populate(['menuId', 'categoryId', 'collectionId', 'colorId', 'featureId', 'styleId', 'subCategoryId']);
+
+const data = allData.filter(item => {
+    return (
+        (!query.menuId || item.menuId.slug === query.menuId) &&
+        (!query.categoryId || item.categoryId.slug === query.categoryId) &&
+        (!query.subCategoryId || item.subCategoryId.slug === query.subCategoryId) &&
+        (!query.collectionId || item.collectionId.slug === query.collectionId) &&
+        (!query.styleId || item.styleId.slug === query.styleId) &&
+        (!query.featureId || item.featureId.slug === query.featureId) &&
+        (!query.colorId || item.colorId.slug === query.colorId) &&
+        (!query.title || titleRegex.test(item.title))
+    );
+});
+
+return data;
+
 }
