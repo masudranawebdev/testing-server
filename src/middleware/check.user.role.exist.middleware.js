31c0410fcf067ca6b6e0550566eb9699f1a28cc1
Hasiful Islam <nazmulbhuyian000@gmail.com>
2024-01-23 15:49:57 +0600

    bug fixed

--- src/middleware/check.user.role.exist.middleware.js
+++ src/middleware/check.user.role.exist.middleware.js
@@ -1,7 +1,7 @@
 const UserModel = require("../app/models/User.model");
 
 // Find A User is Exist ?
-exports.checkAUserExitsForVerify = async (email) => {
-    const FindUser = await UserModel.findOne({ email: email }).select('role email');
+exports.checkAUserExitsForVerify = async (phone) => {
+    const FindUser = await UserModel.findOne({ phone: phone }).select('role phone');
     return FindUser;
 }
